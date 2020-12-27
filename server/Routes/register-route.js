e4e743cedd6efc9f2bd057fec3ffbd5322fdbe96
Abhinav Bhattarai <messibhattarai3@gmail.com>
2020-12-27 10:43:57 +0545

    BE: added Mail Confirmation

--- server/Routes/register-route.js
+++ server/Routes/register-route.js
@@ -3,6 +3,7 @@ import RegistrationModel from '../Models/register-model.js';
 import jwt from 'jsonwebtoken';
 import bcypt from 'bcrypt';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 dotenv.config()
 
@@ -29,6 +30,21 @@ router.post('/', (req, res)=>{
                             Data.save().then((user_data)=>{
                                 jwt.sign(user_data, process.env.JWT_AUTH_KEY, (err, token)=>{
                                     if(!err){
+                                        const Transporter = nodemailer.createTransport({
+                                            service: 'gmail',
+                                            auth: {
+                                                user: process.env.EMAIL,
+                                                pass: process.env.PASS
+                                            }
+                                        })
+
+                                        Transporter.sendMail({
+                                            sender: 'Abhinav Bhattarai',
+                                            to: Email,
+                                            subject: 'LOGIN AUTHENTICATION',
+                                            html: '<h1 style="margin: 10px auto; text-align: center;">Welcome to Meme Tinder</h1><div style="padding: 10px 2%; margin: 10px auto; font-size: 16px">Please confirm your Email to further continue with MemeTinder.</div><a href="https://localhost:3000" style="width: 95%; display:block; margin:10px auto; padding:18px 4%; background-color: #ff374e; color:#fff; font-size:20px; border: none;  border-radius: 10px; margin-top:30px; text-align: center;">Accept to Confirm</a>'
+                                    
+                                        }, (err, info)=>{})
                                         return res.json({data: user_data, token})
                                     }
                                 })
